8ca0e9d18adb65a0fca045dc314d1f3b0fe63b15
ieatseaguls54 <105313116+ieatseaguls54@users.noreply.github.com>
2022-05-17 11:58:49 -0700

    Create dronefly.py

---
+++ dronefly.py
@@ -0,0 +1,35 @@
+from djitellopy import tello
+from time import sleep
+
+# drone takes off
+me = tello.Tello()
+me.connect()
+me.takeoff(120)
+# Forward
+me.move_foward(500)
+me.move_forward(300)
+sleep(.25)
+me.rotate_counter_clockwise(90)
+sleep(.25)
+#sidways shuffle
+me.move_forward(500)
+me.move_forward(300)
+sleep(.25)
+
+me.rotate_counter_clockwise(90)
+sleep(.25)
+#goin home
+me.move_forward(500)
+me.move_forward(300)
+sleep(.25)
+me.rotate_counter_clockwise(90)
+sleep(.25)
+#going to bed
+me.move_forward(500)
+me.move_forward(300)
+sleep(.25)
+me.rotate_counter_clockwise(90)
+sleep(.25)
+#shleep
+me.land()
+# drone moves forward, rotates, waits, and lands.
