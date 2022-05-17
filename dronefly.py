25ef8e858375f86fff01423e64b67d77b3eccb2f
ieatseaguls54 <105313116+ieatseaguls54@users.noreply.github.com>
2022-05-17 12:08:50 -0700

    Update dronefly.py

--- dronefly.py
+++ dronefly.py
@@ -4,16 +4,16 @@ from time import sleep
 # drone takes off
 me = tello.Tello()
 me.connect()
-me.takeoff(120)
+me.takeoff()
+me.move_up(120)
 # Forward
-me.move_foward(500)
+me.move_forward(500)
 me.move_forward(300)
 sleep(.25)
 me.rotate_counter_clockwise(90)
 sleep(.25)
 #sidways shuffle
-me.move_forward(500)
-me.move_forward(300)
+me.move_forward(400)
 sleep(.25)
 
 me.rotate_counter_clockwise(90)
@@ -25,8 +25,7 @@ sleep(.25)
 me.rotate_counter_clockwise(90)
 sleep(.25)
 #going to bed
-me.move_forward(500)
-me.move_forward(300)
+me.move_forward(400)
 sleep(.25)
 me.rotate_counter_clockwise(90)
 sleep(.25)
